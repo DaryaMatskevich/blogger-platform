83161ce3786d694844c735abde913424940e8e28
Darya Matskevich <darramatskevich@gmail.com>
2025-06-28 15:43:35 +0300

    changes

--- app.module.ts
+++ app.module.ts
@@ -5,7 +5,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserAccountsModule } from 'src/modules/user-accounts/userAccounts.module';
 import { CoreModule } from 'src/core/core.module';
 import { TestingModule } from 'src/modules/testing/testing.module';
-import { BloggersPlatformModule } from 'src/modules/bloggers-platform/bloggers-platform.module';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';

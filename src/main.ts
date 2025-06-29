b949954045380f0b2dabd586b59d6cb41b81d045
Darya Matskevich <darramatskevich@gmail.com>
2025-06-29 16:13:31 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../app.module';
+import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
 import { createWriteStream } from 'fs';
 import { get } from 'http';

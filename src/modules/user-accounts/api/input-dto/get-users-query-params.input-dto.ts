2a421c35ecbd55f661e25fa6a0c13e953d155b75
Darya Matskevich <darramatskevich@gmail.com>
2025-06-30 08:06:55 +0300

    changes

--- src/modules/user-accounts/api/input-dto/get-users-query-params.input-dto.ts
+++ src/modules/user-accounts/api/input-dto/get-users-query-params.input-dto.ts
@@ -1,5 +1,5 @@
 
-import { BaseQueryParams } from 'src/core/dto/base.query.params.input.dto';
+import { BaseQueryParams } from '../../../../core/dto/base.query.params.input.dto';
 import { UsersSortBy } from './users-sort-by';
 
 //наследуемся от класса BaseQueryParams, где уже есть pageNumber, pageSize и т.п., чтобы не дублировать эти свойства

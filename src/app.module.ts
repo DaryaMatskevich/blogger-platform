64c6cffe807b2e3d26d50ee461dd73cb3ae1d3e3
Darya Matskevich <darramatskevich@gmail.com>
2025-06-30 15:52:48 +0300

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserAccountsModule } from '././modules/user-accounts/userAccounts.module';
-import { CoreModule } from '././core/core.module';
-import { TestingModule } from '././modules/testing/testing.module';
+import { UserAccountsModule } from './modules/user-accounts/userAccounts.module';
+import { CoreModule } from './core/core.module';
+import { TestingModule } from './modules/testing/testing.module';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -12,13 +12,12 @@ import { join } from 'path';
 
 @Module({
   imports: [
-     
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'swagger-static'),
       serveRoot: process.env.NODE_ENV === 'development' ? '/' : 'api/swagger',
     }),
-     ConfigModule.forRoot(),
-      MongooseModule.forRoot(process.env.MONGO_URL, {
+    MongooseModule.forRoot(process.env.MONGO_URL, {
       dbName: 'nest-bloggers-platform'
     }),//ODO: move to env. will be in the following lessons
 

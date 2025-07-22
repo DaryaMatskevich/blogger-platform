22e7a79c8c2593c12f73939580aa96398e690ed0
Darya Matskevich <darramatskevich@gmail.com>
2025-07-22 15:29:46 +0300

    changes

--- swagger-static/swagger-ui-init.js
+++ swagger-static/swagger-ui-init.js
@@ -1,1 +1,1 @@
-{"timestamp":"2025-07-22T10:55:30.322Z","path":"/swagger/swagger-ui-init.js","message":"Cannot GET /swagger/swagger-ui-init.js","extensions":[],"code":3}
+{"timestamp":"2025-07-22T12:26:48.564Z","path":"/swagger/swagger-ui-init.js","message":"Cannot GET /swagger/swagger-ui-init.js","extensions":[],"code":3}

01bd2fb74f777d4337e57b90b37c473e056c9fbb
Darya Matskevich <darramatskevich@gmail.com>
2025-07-30 19:23:51 +0300

    changes

--- src/modules/bloggers-platform/blogs/api/input-dto/blogs.input-dto.ts
+++ src/modules/bloggers-platform/blogs/api/input-dto/blogs.input-dto.ts
@@ -1,19 +1,19 @@
+import { IsStringWithTrim } from "../../../../../core/decorators/validation/is-string-with-trim";
 import { IsString, IsUrl, Matches, MaxLength} from "class-validator";
+import { descriptionConstraints, nameConstraints, websiteUrlConstraints } from "../../domain/dto/blog.entity";
 
 //dto для боди при создании юзера. Сюда могут быть добавлены декораторы swagger
 export class CreateBlogInputDto {
-  @IsString()
-  @MaxLength(15)
-  name: string;
+  @IsStringWithTrim(nameConstraints.minLength, nameConstraints.maxLength)
+    name: string;
 
-  @IsString()
-  @MaxLength(500)
+  @IsStringWithTrim(descriptionConstraints.minLength, descriptionConstraints.maxLength)
   description: string;
 
  
-  @MaxLength(100)
+  @IsStringWithTrim(websiteUrlConstraints.minLength, websiteUrlConstraints.maxlength)
   @Matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/, {
-    message: 'websiteUrl must be a valid URL starting with https://',
+    message: 'websiteUrl must be a valid URL starting with https:// , field : websiteUrl',
   })
   websiteUrl: string;
 }

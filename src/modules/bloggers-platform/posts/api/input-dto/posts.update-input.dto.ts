1e698cb92c8e55e23d5814ab33b0ea4ff0d94ab5
Darya Matskevich <darramatskevich@gmail.com>
2025-07-31 12:54:40 +0300

    changes

--- src/modules/bloggers-platform/posts/api/input-dto/posts.update-input.dto.ts
+++ src/modules/bloggers-platform/posts/api/input-dto/posts.update-input.dto.ts
@@ -1,6 +1,17 @@
+import { IsStringWithTrim } from "../../../../../core/decorators/validation/is-string-with-trim";
+import { contentConstraints, shortDescriptionConstraints, titleConstraints } from "../../domain/post.entity";
+import { IsString } from "class-validator";
+
 export class UpdatePostDto {
+   @IsStringWithTrim(titleConstraints.minLength, titleConstraints.maxLength)
   title: string;
+
+ @IsStringWithTrim(shortDescriptionConstraints.minLength, shortDescriptionConstraints.maxLength)
   shortDescription: string;
+
+  @IsStringWithTrim(contentConstraints.minLength, contentConstraints.maxLength)
   content: string;
+  
+  @IsString()
   blogId: string;
 }

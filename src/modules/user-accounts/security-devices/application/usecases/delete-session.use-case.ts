46aaec4a212949469409871604b10f095096b501
Darya Matskevich <darramatskevich@gmail.com>
2025-08-19 11:14:45 +0300

    changes

--- src/modules/user-accounts/security-devices/application/usecases/delete-session.use-case.ts
+++ src/modules/user-accounts/security-devices/application/usecases/delete-session.use-case.ts
@@ -1,5 +1,7 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { SessionRepository } from "../../infrastructure/sessions.repository";
+import { DomainException } from "../../../../../core/exeptions/domain-exeptions";
+import { DomainExceptionCode } from "../../../../../core/exeptions/domain-exeption-codes";
 
 export class DeleteSessionCommand {
     constructor(public deviceId: string,
@@ -17,7 +19,12 @@ export class DeleteSessionUseCase
     async execute(command: DeleteSessionCommand) {
         const session = await this.sessionRepository.deleteSessionById(command.deviceId, command.userId);
 
+if (!session) {
 
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: "Session not found"
+      })
         
     }
-}
+    }}

123cd36aa6cf16476ff133d5ad87a2a2737bdf1f
Darya Matskevich <darramatskevich@gmail.com>
2025-10-24 14:16:31 +0300

    changes

--- src/modules/user-accounts/application/auth-usecases/refresh-tokens.usecase.ts
+++ src/modules/user-accounts/application/auth-usecases/refresh-tokens.usecase.ts
@@ -1,70 +1,66 @@
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { JwtService } from "@nestjs/jwt";
-import { Inject } from "@nestjs/common";
-import { ACCESS_TOKEN_STRATEGY_INJECT_TOKEN, REFRESH_TOKEN_STRATEGY_INJECT_TOKEN } from "../../constants/auth-tokens.inject-constants";
-import { SessionRepository } from "../../sessions/infrastructure/sessions.repository";
-import { CryptoService } from "../services/crypto.service";
-import { DomainException } from "../../../../core/exeptions/domain-exeptions";
-import { DomainExceptionCode } from "../../../../core/exeptions/domain-exeption-codes";
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { JwtService } from '@nestjs/jwt';
+import { Inject } from '@nestjs/common';
+import {
+  ACCESS_TOKEN_STRATEGY_INJECT_TOKEN,
+  REFRESH_TOKEN_STRATEGY_INJECT_TOKEN,
+} from '../../constants/auth-tokens.inject-constants';
 
+import { DomainException } from '../../../../core/exeptions/domain-exeptions';
+import { DomainExceptionCode } from '../../../../core/exeptions/domain-exeption-codes';
+import { SessionRepository } from '../../../../modules/user-accounts/sessions/infrastructure/sessions.repository';
 
 export class RefreshTokensCommand {
-    constructor(public userId: string,
-        public deviceId: string,
-        public refreshToken: string) { }
+  constructor(
+    public userId: string,
+    public deviceId: string,
+    public refreshToken: string,
+  ) {}
 }
 
 @CommandHandler(RefreshTokensCommand)
 export class RefreshTokensUseCase
-    implements ICommandHandler<RefreshTokensCommand> {
-    constructor(
-        private sessionsRepository: SessionRepository,
-        
+  implements ICommandHandler<RefreshTokensCommand>
+{
+  constructor(
+    private sessionsRepository: SessionRepository,
 
-        @Inject(ACCESS_TOKEN_STRATEGY_INJECT_TOKEN)
-        private accessTokenContext: JwtService,
+    @Inject(ACCESS_TOKEN_STRATEGY_INJECT_TOKEN)
+    private accessTokenContext: JwtService,
 
-        @Inject(REFRESH_TOKEN_STRATEGY_INJECT_TOKEN)
-        private refreshTokenContext: JwtService,
+    @Inject(REFRESH_TOKEN_STRATEGY_INJECT_TOKEN)
+    private refreshTokenContext: JwtService,
+  ) {}
 
+  async execute(
+    command: RefreshTokensCommand,
+  ): Promise<{ newAccessToken: string; newRefreshToken: string }> {
+    const session = await this.sessionsRepository.findByDeviceId(
+      command.deviceId,
+    );
 
-    ) { }
-
-
-    async execute(command: RefreshTokensCommand): Promise<{ newAccessToken: string, newRefreshToken: string }> {
-
-        const session = await this.sessionsRepository.findByDeviceId(command.deviceId)
-     
-
-        if (session.userId !== command.userId) {
-            throw new DomainException({
-                code: DomainExceptionCode.Unauthorized,
-                message: "Unauthorized"
-            });
-        }
-
-     
-
-        const newAccessToken = this.accessTokenContext.sign(
-            { id: command.userId }
-
-        );
-
-        const newRefreshToken = this.refreshTokenContext.sign({
-            userId: command.userId,
-            deviceId: command.deviceId
-        })
-        
-
-        const now = new Date()
-        session.updateLastActiveDate(now)
-        session.updateRefreshToken(newRefreshToken)
-        await this.sessionsRepository.save(session)
-      
-   
-        return {
-            newAccessToken,
-            newRefreshToken,
-        };
+    if (session.userId !== command.userId) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Unauthorized',
+      });
     }
+
+    const newAccessToken = this.accessTokenContext.sign({ id: command.userId });
+
+    const newRefreshToken = this.refreshTokenContext.sign({
+      userId: command.userId,
+      deviceId: command.deviceId,
+    });
+
+    const now = new Date();
+    session.updateLastActiveDate(now);
+    session.updateRefreshToken(newRefreshToken);
+    await this.sessionsRepository.save(session);
+
+    return {
+      newAccessToken,
+      newRefreshToken,
+    };
+  }
 }

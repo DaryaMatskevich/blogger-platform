74d45183fd82c0022d38464f9b3c128e9fa2bb34
Darya Matskevich <darramatskevich@gmail.com>
2025-10-30 10:34:28 +0300

    changes

--- src/sa/sa.module.ts
+++ src/sa/sa.module.ts
@@ -3,10 +3,11 @@ import { SaUsersController } from '../sa/sa.users-controller';
 import { UserAccountsModule } from '../modules/user-accounts/userAccounts.module';
 import { SaGuard } from '../sa/sa.guard';
 import { CqrsModule } from '@nestjs/cqrs';
+import { DeleteUserUseCase } from '../modules/user-accounts/application/users-usecases/delete-user-usecase';
 
 @Module({
   imports: [CqrsModule, UserAccountsModule], // ← Импортируем модуль, где находится UsersService
   controllers: [SaUsersController],
-  providers: [SaGuard],
+  providers: [SaGuard, DeleteUserUseCase],
 })
 export class SaModule {}

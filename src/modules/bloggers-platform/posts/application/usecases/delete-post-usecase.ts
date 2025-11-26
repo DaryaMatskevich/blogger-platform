3f1f090bc949997cfb8339313a47d53674fc4bd2
Darya Matskevich <darramatskevich@gmail.com>
2025-11-26 14:22:11 +0300

    changes

--- src/modules/bloggers-platform/posts/application/usecases/delete-post-usecase.ts
+++ src/modules/bloggers-platform/posts/application/usecases/delete-post-usecase.ts
@@ -1,5 +1,6 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PostsRepository } from '../../infactructure/posts.repository';
+import { PostsQueryRepository } from '../../infactructure/query/posts.query-repository';
 
 export class DeletePostCommand {
   constructor(public postId: string) {}
@@ -7,10 +8,14 @@ export class DeletePostCommand {
 
 @CommandHandler(DeletePostCommand)
 export class DeletePostUseCase implements ICommandHandler<DeletePostCommand> {
-  constructor(private postsRepository: PostsRepository) {}
+  constructor(
+    private postsRepository: PostsRepository,
+    private postsQueryRepository: PostsQueryRepository,
+  ) {}
 
   async execute(command: DeletePostCommand) {
     const postId = parseInt(command.postId, 10);
+    await this.postsQueryRepository.getByIdOrNotFoundFail(postId);
     await this.postsRepository.delete(postId);
   }
 }

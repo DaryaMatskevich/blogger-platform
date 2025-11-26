8e49cbe86ba7d92bc56d5cd8ffd18f71c91f4b73
Darya Matskevich <darramatskevich@gmail.com>
2025-11-26 11:41:35 +0300

    changes

--- src/modules/testing/testing.module.ts
+++ src/modules/testing/testing.module.ts
@@ -5,9 +5,10 @@ import { User } from './../user-accounts/domain/dto/user.entity';
 import { Session } from './../../modules/user-accounts/sessions/domain/session.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Blog } from '../../modules/bloggers-platform/blogs/domain/dto/blog.entity';
+import { Post } from '../../modules/bloggers-platform/posts/domain/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Session, Blog])],
+  imports: [TypeOrmModule.forFeature([User, Session, Blog, Post])],
   controllers: [TestingController],
   providers: [TestingService],
 })

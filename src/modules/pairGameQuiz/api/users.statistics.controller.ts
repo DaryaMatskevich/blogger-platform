3a080429e7eac3f7056323f853b61d20b146e90f
Darya Matskevich <darramatskevich@gmail.com>
2026-04-07 08:59:38 +0300

    changes

--- src/modules/pairGameQuiz/api/users.statistics.controller.ts
+++ src/modules/pairGameQuiz/api/users.statistics.controller.ts
@@ -1,20 +1,17 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { QueryBus } from '@nestjs/cqrs';
 import { JwtAuthGuard } from '../../../modules/user-accounts/guards/bearer/jwt-auth.guard';
 import { ExtractUserFromRequest } from '../../../modules/user-accounts/guards/decorators/param/extract-user-from-request.decorator';
 import { UserContextDto } from '../../../modules/user-accounts/guards/dto/user-contex.dto';
 import { UsersTopQueryParamsDto } from '../../../modules/pairGameQuiz/api/dto/users.top/users-top-query-params.dto';
 import { GetUsersTopQuery } from '../../../modules/pairGameQuiz/application/queries/get-users-top.query-handler';
 import { UsersTopViewDto } from '../../../modules/pairGameQuiz/api/dto/users.top/users-top-view-dto';
-import { GetMyStatisticQuery } from '@src/modules/pairGameQuiz/application/queries/get-my-statistic.query-handler';
-import { MyStatisticViewDto } from '@src/modules/pairGameQuiz/api/dto/statistic/my-statistic-view.dto';
+import { GetMyStatisticQuery } from '../../../modules/pairGameQuiz/application/queries/get-my-statistic.query-handler';
+import { MyStatisticViewDto } from '../../../modules/pairGameQuiz/api/dto/statistic/my-statistic-view.dto';
 
 @Controller('pair-game-quiz/users')
 export class UsersStatisticsController {
-  constructor(
-    private readonly queryBus: QueryBus,
-    private readonly commandBus: CommandBus,
-  ) {}
+  constructor(private readonly queryBus: QueryBus) {}
 
   @Get('top')
   async getUsersTop(
